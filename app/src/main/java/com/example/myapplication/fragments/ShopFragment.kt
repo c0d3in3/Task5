2dbd3b8039e57ad0c8d043dc1092c9c8c061d8f7
ted3x <tedo.manvelidze@space.ge>
2021-05-09 23:57:37 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/fragments/ShopFragment.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.example.myapplication.R
+
+class ShopFragment : Fragment(R.layout.fragment_shop) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
